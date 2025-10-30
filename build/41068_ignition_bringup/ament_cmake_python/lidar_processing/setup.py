0ff8cddb326e15c11f4ed117573c7101281e7fe2
Talis-K <Talis.Kalnins@student.uts.edu.au>
2025-10-30 10:19:46 +0000

    Add .gitignore for ROS2 workspace

--- build/41068_ignition_bringup/ament_cmake_python/lidar_processing/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages
-from setuptools import setup
-
-setup(
-    name='lidar_processing',
-    version='1.0.3',
-    packages=find_packages(
-        include=('lidar_processing', 'lidar_processing.*')),
-)
